d982df1b0174ab7c602561aa4ff75afe4def393d
Farid Rahimi <farid.rahimi92@gmail.com>
2018-04-15 15:50:54 +0200

    modified webpack config to use webpack-dev-server

---
+++ webpack.dev.conf.js
@@ -0,0 +1,28 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: './dist'
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoEmitOnErrorsPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                include: path.join(__dirname, 'src'),
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    }
+}
